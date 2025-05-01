d7688f9fe63493b8797f2fe36cad280baea3da9b
RaphFT <raph.frem@gmail.com>
2025-05-01 18:09:02 +0200

    Fix Tailwind CSS configuration for production build

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,9 +1,8 @@
-const tailwindcss = require('tailwindcss');
-const autoprefixer = require('autoprefixer');
-
+/** @type {import('postcss').Config} */
 module.exports = {
-  plugins: [
-    tailwindcss,
-    autoprefixer,
-  ],
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+    'postcss-nesting': {},
+  },
 } 
